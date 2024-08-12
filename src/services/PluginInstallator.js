cd1a3fa1fdfbe6ca323ade8f73992da69c65bc33
Vincent Moittié <moittie.vincent@gmail.com>
2024-08-12 10:06:38 +0200

    Update PluginInstallator with new Plugin class

--- src/services/PluginInstallator.js
+++ src/services/PluginInstallator.js
@@ -1,52 +1,25 @@
-const fs = require('fs');
-const { execSync } = require('node:child_process');
+import fs from 'fs';
+import { execSync } from 'node:child_process';
+
+/**
+ * @typedef {import('../models/Plugin').Plugin} Plugin
+ */
 
 /**
  * Execute npm install command and read the package.json file to check the dependencies.
  *
- * @param {Object} plugin - Plugin to install.
- * @param {String} plugin.name - Name of the plugin.
- * @param {String} plugin.url - Url of the plugin.
+ * @param {Plugin} plugin - Plugin to install.
  * @returns {Boolean} True if install command is done and the dependencies are updated,
  * otherwise false.
  */
-function install({ name, url }) {
+export function install(plugin) {
   try {
-    execSync(`npm install -s ${url}`);
+    execSync(`npm install -s ${plugin.packageValue}`);
   } catch (err) {
     return false;
   }
 
   const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 
-  return Object.keys(packageJson.dependencies).includes(name);
+  return Object.keys(packageJson.dependencies).includes(plugin.packageKey);
 }
-
-/**
- * Generate plugin configuration file.
- *
- * @param {Array<Object>} plugins - Array of plugin object.
- */
-function generateConfigurationFile(plugins) {
-  const content = {
-    plugins: plugins.map((plugin) => plugin.name),
-  };
-
-  if (fs.existsSync('plugin.config.json')) {
-    const pluginConfig = JSON.parse(fs.readFileSync('plugin.config.json', 'utf8'));
-    const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
-
-    content.plugins = [...new Set(
-      content.plugins
-        .concat(pluginConfig.plugins)
-        .filter((name) => Object.keys(packageJson.dependencies).includes(name)),
-    )];
-  }
-
-  fs.writeFileSync('plugin.config.json', JSON.stringify(content, null, 2));
-}
-
-module.exports = {
-  install,
-  generateConfigurationFile,
-};
