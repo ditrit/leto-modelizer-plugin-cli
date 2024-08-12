040fccc89f6928367225b82825b99cc6f4a9ba20
Vincent Moittié <moittie.vincent@gmail.com>
2024-08-12 15:24:09 +0200

    Update prompts for multiple actions

--- src/prompts/Plugin.js
+++ src/prompts/Plugin.js
@@ -1,10 +1,10 @@
 import prompts from 'prompts';
 
-export async function getOfficialPlugins(plugins) {
+export async function getOfficialPlugins(plugins, action) {
   return (await prompts({
     type: 'multiselect',
     name: 'type',
-    message: 'Choose plugin(s) to install',
+    message: `Choose plugin(s) to ${action}`,
     choices: plugins.map((plugin) => ({
       title: `${plugin.displayName} (${plugin.version})`,
       value: plugin,
@@ -13,11 +13,11 @@ export async function getOfficialPlugins(plugins) {
   })).type;
 }
 
-export async function confirmInstallation(plugin) {
+export async function confirmAction(plugin, action) {
   return (await prompts({
     type: 'toggle',
     name: 'value',
-    message: `Install ${plugin.displayName} (${plugin.version}) ?`,
+    message: `Confirm: ${action} plugin ${plugin.displayName} (${plugin.version}) ?`,
     initial: true,
     active: 'yes',
     inactive: 'no',
