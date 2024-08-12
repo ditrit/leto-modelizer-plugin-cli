ea301cfeb14c66e88d937079b901dcbb3f087f86
Vincent Moittié <moittie.vincent@gmail.com>
2024-08-12 11:50:38 +0200

    Update PluginInstallator to copy public folder too

--- src/services/PluginInstallator.js
+++ src/services/PluginInstallator.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import { execSync } from 'node:child_process';
+import chalk from 'chalk';
 
 /**
  * @typedef {import('../models/Plugin').Plugin} Plugin
@@ -21,5 +22,21 @@ export function install(plugin) {
 
   const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 
-  return Object.keys(packageJson.dependencies).includes(plugin.packageKey);
+  if (!Object.keys(packageJson.dependencies).includes(plugin.packageKey)) {
+    return false;
+  }
+
+  const srcDir = `./node_modules/${plugin.packageKey}/public/`;
+  const distDir = `./public/plugins/${plugin.name}/`;
+
+  if (!fs.existsSync(srcDir)) {
+    console.log(`\n${chalk.red('✘')} Plugin '${plugin.name}': No icons and models are imported.`);
+    return true;
+  }
+
+  fs.cpSync(srcDir, distDir, { recursive: true });
+
+  console.log(`\n${chalk.green('✔')} Plugin '${plugin.name}': Icons and models are imported.`);
+
+  return true;
 }
